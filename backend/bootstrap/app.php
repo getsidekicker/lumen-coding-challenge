37e8e56c05e78b848ad6bb02d7f2c4b4636a8abc
Daniel Breves <djbreves@gmail.com>
2021-06-09 22:32:49 +1000

    Add cors middleware

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -80,6 +80,10 @@ $app->configure('app');
 //     'auth' => App\Http\Middleware\Authenticate::class,
 // ]);
 
+$app->middleware([
+    App\Http\Middleware\Cors::class
+]);
+
 /*
 |--------------------------------------------------------------------------
 | Register Service Providers
